dc26f55d7a842b27968504aaa60790ecd6a6d44d
Le-atuan <lanhtuan65@gmail.com>
2026-07-16 18:47:54 +0700

    Update fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/vi",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
